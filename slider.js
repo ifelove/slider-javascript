e0ce424183cc2807a9ffcc6c4034e4dd92b8abbc
Ifeoluwa <ifel1939@gmail.com>
2022-11-26 00:33:00 +0100

    slider using basic javascript

---
+++ slider.js
@@ -0,0 +1,84 @@
+let images=[ { img:"animal/animal.jpg",
+               position:"1"},
+
+               {img:"animal/animal2.jpg",
+               position:"2"},
+
+               {img:"animal/animal3.jpg",
+               position:"3"},
+               
+            {img:"animal/animal4.jpg",
+               position:"4"},
+            
+            ]
+
+
+let currentImage=0
+var slideImage=document.querySelector(".slide-img")
+let imagePosition=document.querySelector(".image-position")
+let slide=document.querySelector(".slide")
+let prevBtn=document.querySelector(".prev")
+let nextBtn=document.querySelector(".next")
+let autoBtn=document.querySelector(".auto")
+let indicators=document.querySelectorAll(".indicators span")
+
+
+window.addEventListener("DOMContentLoaded",DisplayImage(currentImage))
+
+
+/*
+for(i=0;i<indicators.length;i++){
+indicators[i].style.background="white"
+}
+indicators[currentImage].style.background="red"
+*/
+
+function DisplayImage(picture){
+    let item=images[picture]
+slideImage.src=item.img
+imagePosition.innerText=item.position
+
+
+indicators.forEach((indicator)=>{
+    indicator.style.background="none"
+})
+indicators[picture].style.background="red"
+
+}
+
+nextBtn.addEventListener("click",()=>{ 
+    //stop()
+    currentImage=currentImage+1 
+if(currentImage>images.length-1){currentImage=0}
+
+        DisplayImage(currentImage)
+})
+
+prevBtn.addEventListener("click",()=>{ 
+//stop()
+    currentImage=currentImage-1 
+if(currentImage<0){currentImage=images.length-1}
+
+        DisplayImage(currentImage)
+})
+
+function timer(){
+    currentImage=currentImage+1
+    if(currentImage>images.length-1){currentImage=0}
+    if(currentImage<0){currentImage=images.length-1}
+    DisplayImage(currentImage)
+}
+
+ // function stop(){
+  //  clearInterval(intervalid)
+ // }
+
+function start(){
+  intervalid=  setInterval(timer,3000)}
+
+
+autoBtn.addEventListener("click",start)
+
+
+function onclickImage(e){DisplayImage(currentImage=e)
+}
